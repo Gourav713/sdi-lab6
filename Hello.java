c86a2813fd1d683efa25546c5837302d443e3ec2
Gourav Verma <gouravpatel344@gmail.com>
2026-02-16 15:51:13 +0530

    2nd-commit

--- Hello.java
+++ Hello.java
@@ -1,5 +1,8 @@
 public class Hello {
     public static void main(String[] args) {
+        int a = 20;
+        int b = 10;
+        System.out.println("Total is " + (a + b));
         System.out.println("Hello, World! rffrom Java");
     }
 }
