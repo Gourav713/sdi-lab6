7d2b8ab95c23ea1e5508743dbf20582a0f5519e9
Gourav Verma <gouravpatel344@gmail.com>
2026-02-16 15:54:29 +0530

    3rd-commit

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         int a = 20;
         int b = 10;
-        System.out.println("Total is " + (a + b));
-        System.out.println("Hello, World! rffrom Java");
+        System.out.println("Total is " + (a - b));
+        System.out.println("Hello Jenkins");
     }
 }
